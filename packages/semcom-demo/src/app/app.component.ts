251ac7ea02439f7104af372c83b39975f7858b22
Wouter Termont <woutermont@gmail.com>
2021-02-15 14:29:05 +0100

    feat: i18n support
    Signed-off-by: Wouter Termont <woutermont@gmail.com>

--- packages/semcom-demo/src/app/app.component.ts
+++ packages/semcom-demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'demo-root',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
 
 }
