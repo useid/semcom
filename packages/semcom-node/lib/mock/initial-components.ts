e6f32274461803d9ba015533985142adcea616b3
Arne Vandoorslaer <arne@digita.ai>
2021-02-17 10:55:35 +0100

    feat: changed initial components

--- packages/semcom-node/lib/mock/initial-components.ts
+++ packages/semcom-node/lib/mock/initial-components.ts
@@ -1,36 +1,8 @@
 import { ComponentMetadata } from '@digita-ai/semcom-core';
 
 export const initialComponents = [
-  {
-    uri: 'foo1/bar',
-    name: 'test1',
-    label: 'test1',
-    author: 'test1',
-    version: 'test1',
-    latest: false,
-  } as ComponentMetadata,
-  {
-    uri: 'foo2/bar',
-    name: 'test2',
-    label: 'test2',
-    author: 'test2',
-    version: 'test2',
-    latest: true,
-  } as ComponentMetadata,
-  {
-    uri: 'foo3/bar',
-    name: 'test3',
-    label: 'test3',
-    author: 'test3',
-    version: 'test3',
-    latest: false,
-  } as ComponentMetadata,
-  {
-    uri: 'foo4/bar',
-    name: 'test4',
-    label: 'test4',
-    author: 'test4',
-    version: 'test4',
-    latest: true,
-  } as ComponentMetadata,
-] as ComponentMetadata[];
+  new ComponentMetadata('foo1/bar', 'test1', 'test1', 'test1', 'test1', false),
+  new ComponentMetadata('foo2/bar', 'test2', 'test2', 'test2', 'test2', true),
+  new ComponentMetadata('foo3/bar', 'test3', 'test3', 'test3', 'test3', false),
+  new ComponentMetadata('foo4/bar', 'test4', 'test4', 'test4', 'test4', true),
+];
