022076ce754c5a86f56902e58eb28192637cd6ee
Wouter Termont <woutermont@gmail.com>
2021-02-19 16:57:54 +0100

    feat: component model rdf.js dataset
    Signed-off-by: Wouter Termont <woutermont@gmail.com>

--- packages/semcom-core/lib/component/models/component.model.ts
+++ packages/semcom-core/lib/component/models/component.model.ts
@@ -1,9 +1,10 @@
 import { ComponentMetadata } from './component-metadata.model';
+import { DatasetIndexed } from 'rdf-dataset-indexed/dataset';
 
 export interface Component {
   
   metadata: ComponentMetadata;
 
-  setData(data): void;
+  rdfData: DatasetIndexed;
 
 }
