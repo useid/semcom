0127c8365723d362d200cd393d99b8f9aee09682
Arne Vandoorslaer <arne@digita.ai>
2021-04-29 17:05:28 +0200

    feat: made changes to NodeHttpServer handler structure

--- packages/semcom-node/lib/main.ts
+++ packages/semcom-node/lib/main.ts
@@ -13,7 +13,7 @@ const launch = async () => {
   await manager.configRegistry.register(configPath);
 
   const server: NodeHttpServer = await manager.instantiate(
-    'urn:handlersjs-http:default:NodeHttpServer',
+    'urn:semcom-node:default:NodeHttpServer',
   );
 
   server.start();
