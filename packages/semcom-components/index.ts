d50461afe3a6d10a2f2991a14773deae244476a6
Wouter Termont <woutermont@gmail.com>
2021-05-29 09:26:15 +0200

    event based componente (WIP)

--- packages/semcom-components/index.ts
+++ packages/semcom-components/index.ts
@@ -1,40 +1,64 @@
-import PayslipComponent from './components/payslip';
-import ProfileComponent from './components/profile';
+import * as N3 from 'n3';
+// import PayslipComponent from './components/payslip';
+// import ProfileComponent from './components/profile';
+import ProfileEventComponent from './components/profile_events';
 
-customElements.define('profile-component', ProfileComponent);
-customElements.define('payslip-component', PayslipComponent);
+// customElements.define('profile-component', ProfileComponent);
+customElements.define('profile-event-component', ProfileEventComponent);
+// customElements.define('payslip-component', PayslipComponent);
 declare global {
   interface HTMLElementTagNameMap {
-    'profile-component': ProfileComponent;
-    'payslip-component': PayslipComponent;
+    // 'profile-component': ProfileComponent;
+    'profile-event-component': ProfileEventComponent;
+    // 'payslip-component': PayslipComponent;
   }
 }
 
-fetch('./testdata/profile.txt').then((response) => {
+const parser = new N3.Parser();
 
-  response.text().then((profileFile) => {
+document.addEventListener('semcom-data-request', (event: CustomEvent & { detail: { uri: string } }) => {
 
-    const mockFetchProfile = () => Promise.resolve(new Response(profileFile));
-    const profile = document.createElement('profile-component');
-    profile.data('i-dont-matter', mockFetchProfile);
-    document.body.appendChild(profile);
+  console.log(event);
 
-  });
+  fetch('./testdata/profile.txt').then((response) => response.text().then((profileText) => {
 
-});
+    const quads = parser.parse(profileText);
+    event.target.dispatchEvent(new CustomEvent('semcom-data-response', { detail: quads }));
+    // if nested components are used, event.target should be event.composedPath[0], but this only works with open shadow roots (like litelement)
 
-fetch('./testdata/singlePayslip.txt').then((response) => {
-
-  response.text().then((payslipFile) => {
-
-    const mockFetchPayslip = () => Promise.resolve(new Response(payslipFile));
-    const payslip = document.createElement('payslip-component');
-    payslip.data('i-dont-matter', mockFetchPayslip);
-    document.body.appendChild(payslip);
-
-  });
+  }));
 
 });
+
+const event_profile = document.createElement('profile-event-component');
+event_profile.setAttribute('entry', 'http://example.com');
+document.body.appendChild(event_profile);
+
+// fetch('./testdata/profile.txt').then((response) => {
+
+//   response.text().then((profileFile) => {
+
+//     const mockFetchProfile = () => Promise.resolve(new Response(profileFile));
+//     const profile = document.createElement('profile-component');
+//     profile.data('i-dont-matter', mockFetchProfile);
+//     document.body.appendChild(profile);
+
+//   });
+
+// });
+
+// fetch('./testdata/singlePayslip.txt').then((response) => {
+
+//   response.text().then((payslipFile) => {
+
+//     const mockFetchPayslip = () => Promise.resolve(new Response(payslipFile));
+//     const payslip = document.createElement('payslip-component');
+//     payslip.data('i-dont-matter', mockFetchPayslip);
+//     document.body.appendChild(payslip);
+
+//   });
+
+// });
 
 export * from './components/profile';
 export * from './components/payslip';
