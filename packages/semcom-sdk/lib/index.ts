886c7524b57b7b2b17fd7db06611693e183e8ba4
Wouter Janssens <wouter@digita.ai>
2021-06-02 17:20:57 +0200

    fix: change export path of query service

--- packages/semcom-sdk/lib/index.ts
+++ packages/semcom-sdk/lib/index.ts
@@ -1,4 +1,4 @@
 export * from './discovery';
-export * from './services/query-component-remote.service';
+export * from './component/services/query-component-remote.service';
 export * from './component/models/component-events.model';
 export * from './component/services/register-component.service';
