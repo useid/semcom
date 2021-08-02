401a31512463035a9c1fc5d48ba2faeb705c5ff2
Jonas Bovyn <bovynjonas@gmail.com>
2021-08-02 10:09:04 +0200

    test: new tests for register-component

--- packages/semcom-sdk/lib/component/services/register-component.service.spec.ts
+++ packages/semcom-sdk/lib/component/services/register-component.service.spec.ts
@@ -5,11 +5,28 @@ describe('RegisterComponentService', () => {
 
   let service: RegisterComponentService;
 
+  const addedUris = new Set();
+
   beforeEach(() => {
 
     service = new RegisterComponentService();
 
   });
+
+  afterEach(() => {
+
+    jest.resetAllMocks();
+    addedUris.clear();
+
+  });
+
+  const mockEvalAndDefine = () => {
+
+    global.eval = jest.fn(async (url) => ({ 'default': {} }));
+    customElements.define = jest.fn((tag, html) => { addedUris.add(tag); });
+    customElements.get = jest.fn((tag) => addedUris.has(tag) ? {} as CustomElementConstructor : undefined);
+
+  };
 
   it('should be correctly instantiated', () => {
 
@@ -93,6 +110,118 @@ describe('RegisterComponentService', () => {
     } as ComponentMetadata;
 
     await expect(service.isRegistered(mockComponent)).rejects.toThrow();
+
+  });
+
+  it('should generate a correct tag', async () => {
+
+    mockEvalAndDefine();
+
+    const mockComponent = {
+      uri: 'https://www.google.com/test.js',
+      tag: 'component',
+    } as ComponentMetadata;
+
+    const tag = await service.register(mockComponent);
+    expect(tag.startsWith('semcom-component-')).toBe(true);
+
+  });
+
+  it('should eval and define a component', async () => {
+
+    mockEvalAndDefine();
+
+    const mockComponent = {
+      uri: 'https://www.google.com/test.js',
+      tag: 'component',
+    } as ComponentMetadata;
+
+    await service.register(mockComponent);
+    expect(global.eval).toBeCalledTimes(1);
+    expect(service.isRegistered(mockComponent)).toBeTruthy();
+
+  });
+
+  it('should not reimport an existing component if it was already defined', async () => {
+
+    mockEvalAndDefine();
+
+    const mockComponent = {
+      uri: 'https://www.google.com/test.js',
+      tag: 'component',
+    } as ComponentMetadata;
+
+    await service.register(mockComponent);
+    await service.register(mockComponent);
+
+    expect(global.eval).toBeCalledTimes(1);
+    expect(customElements.define).toBeCalledTimes(1);
+
+  });
+
+  it('should only define a tag once, if two are imported at the same time', async () => {
+
+    mockEvalAndDefine();
+
+    const mockComponent = {
+      uri: 'https://www.google.com/test.js',
+      tag: 'component',
+    } as ComponentMetadata;
+
+    await Promise.all([ service.register(mockComponent), service.register(mockComponent) ]);
+    expect(customElements.define).toBeCalledTimes(1);
+
+  });
+
+  it('should return different html tags for components with different tags', async () => {
+
+    mockEvalAndDefine();
+
+    const mockComponent = {
+      uri: 'https://www.google.com/test.js',
+      tag: 'component',
+    } as ComponentMetadata;
+
+    const mockComponent2 = {
+      uri: 'https://www.google.com/test2.js',
+      tag: 'component2',
+    } as ComponentMetadata;
+
+    const tag = await service.register(mockComponent);
+    await expect(service.register(mockComponent2)).resolves.not.toBe(tag);
+
+  });
+
+  it('should return a different html Tag for components with the same tag but a different URI', async () => {
+
+    mockEvalAndDefine();
+
+    const mockComponent = {
+      uri: 'https://www.google.com/test.js',
+      tag: 'component',
+    } as ComponentMetadata;
+
+    const mockComponent2 = {
+      uri: 'https://www.google.com/test2.js',
+      tag: 'component',
+    } as ComponentMetadata;
+
+    const tag = await service.register(mockComponent);
+    await expect(service.register(mockComponent2)).resolves.not.toBe(tag);
+
+  });
+
+  it('should return the same tag if a component is imported twice', async () => {
+
+    mockEvalAndDefine();
+
+    const mockComponent = {
+      uri: 'https://www.google.com/test.js',
+      tag: 'component',
+    } as ComponentMetadata;
+
+    const tag = await service.register(mockComponent);
+    await expect(service.register(mockComponent)).resolves.toBe(tag);
 
   });
 
