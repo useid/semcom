9716671a4b8830f4fe9b440e3980c52ca808de04
Jonas Bovyn <bovynjonas@gmail.com>
2021-08-09 15:12:48 +0200

    feat: fromQuads

--- packages/semcom-node/lib/component/services/component-transformer.service.ts
+++ packages/semcom-node/lib/component/services/component-transformer.service.ts
@@ -1,5 +1,18 @@
-import { Quad, NamedNode, Literal } from 'n3';
+import { Quad, NamedNode, Literal, Term } from 'n3';
 import { ComponentMetadata, LoggerService } from '@digita-ai/semcom-core';
+
+const digitaPrefix = 'http://semcom.digita.ai/voc#';
+
+const requiredPredicates: ReadonlySet<string> = new Set([
+  `${digitaPrefix}label`,
+  `${digitaPrefix}description`,
+  `${digitaPrefix}author`,
+  `${digitaPrefix}version`,
+  `${digitaPrefix}latest`,
+  `${digitaPrefix}tag`,
+  `${digitaPrefix}shape`,
+  `http://www.w3.org/1999/02/22-rdf-syntax-ns#type`,
+]);
 
 /** Service that transforms Components */
 export class ComponentTransformerService {
@@ -22,16 +35,16 @@ export class ComponentTransformerService {
     }
 
     const shapeQuads = component.shapes.map((shape) =>
-      new Quad(new NamedNode(component.uri), new NamedNode('http://semcom.digita.ai/voc#shape'), new Literal(shape)));
+      new Quad(new NamedNode(component.uri), new NamedNode(`${digitaPrefix}shape`), new NamedNode(shape)));
 
     const quads = [
-      new Quad(new NamedNode(component.uri), new NamedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), new NamedNode('http://semcom.digita.ai/voc#component'),),
-      new Quad(new NamedNode(component.uri), new NamedNode('http://semcom.digita.ai/voc#label'), new Literal(component.label)),
-      new Quad(new NamedNode(component.uri), new NamedNode('http://semcom.digita.ai/voc#description'),  new Literal(component.description)),
-      new Quad(new NamedNode(component.uri), new NamedNode('http://semcom.digita.ai/voc#author'), new Literal(component.author)),
-      new Quad(new NamedNode(component.uri), new NamedNode('http://semcom.digita.ai/voc#version'), new Literal(component.version)),
-      new Quad(new NamedNode(component.uri), new NamedNode('http://semcom.digita.ai/voc#latest'), new Literal(String(component.latest))),
-      new Quad(new NamedNode(component.uri), new NamedNode('http://semcom.digita.ai/voc#tag'), new Literal(component.tag)),
+      new Quad(new NamedNode(component.uri), new NamedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), new NamedNode('http://semcom.digita.ai/voc#component')),
+      new Quad(new NamedNode(component.uri), new NamedNode(`${digitaPrefix}label`), new Literal(component.label)),
+      new Quad(new NamedNode(component.uri), new NamedNode(`${digitaPrefix}description`),  new Literal(component.description)),
+      new Quad(new NamedNode(component.uri), new NamedNode(`${digitaPrefix}author`), new Literal(component.author)),
+      new Quad(new NamedNode(component.uri), new NamedNode(`${digitaPrefix}version`), new Literal(component.version)),
+      new Quad(new NamedNode(component.uri), new NamedNode(`${digitaPrefix}latest`), new Literal(String(component.latest))),
+      new Quad(new NamedNode(component.uri), new NamedNode(`${digitaPrefix}tag`), new Literal(component.tag)),
       ...shapeQuads,
     ];
 
@@ -57,14 +70,70 @@ export class ComponentTransformerService {
   }
 
   /**
-   * Transforms quads to a Component
+   * Transforms quads to a Components
+   *
+   * @param quads The quads to transform
+   * @param subject The URI of the componentMetadata
+   */
+  fromQuadsOne(quads: Quad[], uri: string): ComponentMetadata {
+
+    this.logger.log('debug', 'Transforming quads into component', { quads });
+
+    // maps all predicates for the given URI to its corresponding object
+    const uriTriples = new Map<string, string[]>([ ...requiredPredicates ].map((p) => [ p, [] ]));
+
+    quads
+      .filter((quad) => quad.subject.value === uri)
+      .forEach((element) => uriTriples.get(element.predicate.value)?.push(element.object.value));
+
+    if ([ ...uriTriples.values() ].some((preds) => preds.length === 0)) {
+
+      throw new Error('Some ComponentMetadata predicates were missing while parsing quads.');
+
+    }
+
+    if ([ ...uriTriples.values() ].some((preds) => preds.length > 1)) {
+
+      throw new Error('Too many ComponentMetadata predicates were provided while parsing quads.');
+
+    }
+
+    const latest = uriTriples.get(`${digitaPrefix}latest`)?.[0];
+
+    if (latest !== 'true' && latest !== 'false') {
+
+      throw new Error(`'${digitaPrefix}latest' should be 'true' or 'false'`);
+
+    }
+
+    return {
+      uri,
+      label: uriTriples.get(`${digitaPrefix}latest`)?.[0] ?? '',
+      description: uriTriples.get(`${digitaPrefix}description`)?.[0] ?? '',
+      author: uriTriples.get(`${digitaPrefix}author`)?.[0] ?? '',
+      tag: uriTriples.get(`${digitaPrefix}tag`)?.[0] ?? '',
+      version: uriTriples.get(`${digitaPrefix}version`)?.[0] ?? '',
+      latest: uriTriples.get(`${digitaPrefix}latest`)?.[0] === 'true',
+      shapes: uriTriples.get(`${digitaPrefix}shape`) ?? [],
+    };
+
+  }
+
+  /**
+   * Transforms quads to Components
    *
    * @param quads The quads to transform
    */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  fromQuads(quads: Quad[]): ComponentMetadata {
+  fromQuads(quads: Quad[]): ComponentMetadata[] {
 
-    throw new Error('Not implemented');
+    const typePredicate = new NamedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type');
+    const componentObject = new NamedNode('http://semcom.digita.ai/voc#component');
+
+    return quads
+      .filter((quad) => quad.predicate.equals(typePredicate) && quad.object.equals(componentObject))
+      .map((quad) => quad.subject.value)
+      .map((uri) => this.fromQuadsOne(quads, uri));
 
   }
 
