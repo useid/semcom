23ae680b24be767f2e864d9ec05e996d2e76d711
Jonas Bovyn <bovynjonas@gmail.com>
2021-08-10 11:41:10 +0200

    fix: make sure tags with same URI are the same (#141)
    * fix: make sure tags with same URI are the same
    
    * fix: use hash-based tag instead of time-based
    
    * fix: avoid javascript injection
    
    * test: new tests for register-component
    
    * chore: code reformat of tests
    
    * fix: test no longer throws segmentation fault
    
    Co-authored-by: AbelVandenBriel <abel.van.den.briel@digita.ai>
    Co-authored-by: Arne Vandoorslaer <arne@digita.ai>

--- packages/semcom-sdk/lib/component/services/register-component.service.spec.ts
+++ packages/semcom-sdk/lib/component/services/register-component.service.spec.ts
@@ -3,11 +3,35 @@ import { RegisterComponentService } from './register-component.service';
 
 describe('RegisterComponentService', () => {
 
+  const testComponent = {
+    uri: 'https://www.google.com/test.js',
+    tag: 'component',
+  } as ComponentMetadata;
+
+  let testComponent2: ComponentMetadata;
+
   let service: RegisterComponentService;
+
+  const addedUris = new Set();
 
   beforeEach(() => {
 
     service = new RegisterComponentService();
+
+    testComponent2 = {
+      uri: 'https://www.google.com/test2.js',
+      tag: 'component2',
+    } as ComponentMetadata;
+
+    global.eval = jest.fn(async (url) => ({ 'default': {} }));
+    customElements.define = jest.fn((tag, html) => { addedUris.add(tag); });
+    customElements.get = jest.fn((tag) => addedUris.has(tag) ? {} as CustomElementConstructor : undefined);
+
+  });
+
+  afterEach(() => {
+
+    addedUris.clear();
 
   });
 
@@ -66,12 +90,14 @@ describe('RegisterComponentService', () => {
 
   it('should throw error when componentMetadata.uri is not found', async () => {
 
+    global.eval = jest.fn(async (url) => { throw new Error('Not found'); });
+
     const mockComponent = {
       uri: './../../mock/non-working-component.ts',
       tag: 'app-component-4',
     } as ComponentMetadata;
 
-    await expect(service.register(mockComponent)).rejects.toThrow();
+    await expect(service.register(mockComponent)).rejects.toThrow('Failed to import or register componentMetadata: Error: Not found');
 
   });
 
@@ -93,6 +119,61 @@ describe('RegisterComponentService', () => {
     } as ComponentMetadata;
 
     await expect(service.isRegistered(mockComponent)).rejects.toThrow();
+
+  });
+
+  it('should generate a correct tag', async () => {
+
+    const tag = await service.register(testComponent);
+    expect(tag.startsWith('semcom-component-')).toBe(true);
+
+  });
+
+  it('should eval and define a component', async () => {
+
+    await service.register(testComponent);
+    expect(global.eval).toBeCalledTimes(1);
+    expect(service.isRegistered(testComponent)).toBeTruthy();
+
+  });
+
+  it('should not reimport an existing component if it was already defined', async () => {
+
+    await service.register(testComponent);
+    await service.register(testComponent);
+
+    expect(global.eval).toBeCalledTimes(1);
+    expect(customElements.define).toBeCalledTimes(1);
+
+  });
+
+  it('should only define a tag once, if two are imported at the same time', async () => {
+
+    await Promise.all([ service.register(testComponent), service.register(testComponent) ]);
+    expect(customElements.define).toBeCalledTimes(1);
+
+  });
+
+  it('should return different html tags for components with different tags', async () => {
+
+    const tag = await service.register(testComponent);
+    await expect(service.register(testComponent2)).resolves.not.toBe(tag);
+
+  });
+
+  it('should return a different html Tag for components with the same tag but a different URI', async () => {
+
+    testComponent2.tag = 'component';
+
+    const tag = await service.register(testComponent);
+    await expect(service.register(testComponent2)).resolves.not.toBe(tag);
+
+  });
+
+  it('should return the same tag if a component is imported twice', async () => {
+
+    const tag = await service.register(testComponent);
+    await expect(service.register(testComponent)).resolves.toBe(tag);
 
   });
 
