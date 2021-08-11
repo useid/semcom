ea8bb4837821687be7c7a9e32f54dd7a0896f754
Jonas Bovyn <bovynjonas@gmail.com>
2021-08-11 10:53:08 +0200

    feat: component pod store working implementation

--- packages/semcom-node/lib/store/services/component-pod-store.service.ts
+++ packages/semcom-node/lib/store/services/component-pod-store.service.ts
@@ -1,4 +1,5 @@
 import { ComponentMetadata } from '@digita-ai/semcom-core';
+import jsSHA from 'jssha';
 import { Parser, Quad } from 'n3';
 import { ComponentTransformerService } from '../../component/services/component-transformer.service';
 import { ComponentStore } from './component-store.service';
@@ -8,34 +9,40 @@ export class ComponentPodStore extends ComponentStore {
   constructor(
     private readonly uri: string,
     private readonly transformer: ComponentTransformerService
-  ) {
+  ) { super(); }
 
-    super();
+  private async fetchAllQuads(): Promise<Quad[]> {
+
+    const body = await fetch(this.uri, { headers: { 'Accept': 'text/turtle' } })
+      .then((response) => response.text());
+
+    const components = new Parser({ format: 'Turtle' }).parse(body)
+      .filter((q) => q.object.value === 'http://www.w3.org/ns/ldp#Resource')
+      .filter((q) => q.subject.value !== '')
+      .map((q) => q.subject.value);
+
+    const allQuads: Quad[][] = await Promise.all(([ ... components ]).map((component) => this.fetchQuads(component)));
+
+    return allQuads.flat();
 
   }
 
-  private async fetchQuads(): Promise<Quad[]> {
+  private async fetchQuads(metadataName: string): Promise<Quad[]> {
 
-    return fetch(this.uri, { headers: { 'Accept': 'text/turtle' } })
+    return fetch(`${this.uri}${metadataName}`, { headers: { 'Accept': 'text/turtle' } })
       .then((response) => response.text())
-      .then((body) =>   new Parser({ format: 'Turtle' }).parse(body));
+      .then((text) =>  new Parser({ format: 'Turtle' }).parse(text));
 
   }
 
   async all(): Promise<ComponentMetadata[]> {
 
-    const quads = await this.fetchQuads();
+    const quads = await this.fetchAllQuads();
 
     return this.transformer.fromQuads(quads);
 
   }
-  async get(uri: string): Promise<ComponentMetadata[]> {
 
-    const quads = await this.fetchQuads();
-
-    return [ this.transformer.fromQuadsOne(quads, uri) ];
-
-  }
   async save(components: ComponentMetadata[]): Promise<ComponentMetadata[]> {
 
     throw new Error('Method not implemented.');
