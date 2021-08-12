84052c750fa324075867530f981662d6427f7642
Jonas Bovyn <bovynjonas@gmail.com>
2021-08-12 12:23:01 +0200

    feat: pod store for component metadata (#161)
    * feat: fromQuads
    
    * feat: component pod store
    
    * fix: lagg when loading semcom demo app
    
    * feat: add logging for parser
    
    * chore: remove unnecessary brackets
    
    * feat: remove caching mechanism
    
    * feat: locally hosted metadata for start:dev
    
    * fix: fix creation of n3 quads
    
    * fix: typo that caused invlid label
    
    * test: tests fromQuads and fromQuadsOne
    
    * feat: split up component metadatas in ttl files
    
    * build: add jssha dependency
    
    * feat: remove get method from componentstore WIP
    
    * feat: component pod store working implementation
    
    * build: remove jssha
    
    * feat: pod sync service
    
    * build: setup local podsync service
    
    * fix: fix error when pod is offline
    
    * build: start pod sync service & meta in initialdata
    
    * build: setup 2 node sync for metadata
    
    * build: update handlersjs
    
    * build: start:sync:dev script in other packages
    
    * build: config to use inmemorystore
    
    * fix: readd missing .acl file in pods
    
    * build: coveragethresholds
    
    * build: fix threshold again
    
    * chore: code improvements
    
    * chore: code improvements 2
    
    * build: remove start:memory
    
    Co-authored-by: Wouter Termont <woutermont@gmail.com>

--- packages/semcom-node/lib/component/services/component-transformer.service.ts
+++ packages/semcom-node/lib/component/services/component-transformer.service.ts
@@ -1,6 +1,20 @@
 import { DataFactory, Quad } from 'n3';
 import { ComponentMetadata, LoggerService } from '@digita-ai/semcom-core';
-const { namedNode, literal, quad } = DataFactory;
+
+const { namedNode, literal, quad: createQuad } = DataFactory;
+
+const digitaPrefix = 'http://semcom.digita.ai/voc#';
+
+const requiredPredicates: ReadonlySet<string> = new Set([
+  `${digitaPrefix}label`,
+  `${digitaPrefix}description`,
+  `${digitaPrefix}author`,
+  `${digitaPrefix}version`,
+  `${digitaPrefix}latest`,
+  `${digitaPrefix}tag`,
+  `${digitaPrefix}shape`,
+  `http://www.w3.org/1999/02/22-rdf-syntax-ns#type`,
+]);
 
 /** Service that transforms Components */
 export class ComponentTransformerService {
@@ -23,16 +37,16 @@ export class ComponentTransformerService {
     }
 
     const shapeQuads = component.shapes.map((shape) =>
-      quad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#shape'), namedNode(shape)));
+      createQuad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#shape'), namedNode(shape)));
 
     const quads = [
-      quad(namedNode(component.uri), namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), namedNode('http://semcom.digita.ai/voc#component'),),
-      quad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#label'), literal(component.label)),
-      quad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#description'),  literal(component.description)),
-      quad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#author'), literal(component.author)),
-      quad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#version'), literal(component.version)),
-      quad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#latest'), literal(String(component.latest))),
-      quad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#tag'), literal(component.tag)),
+      createQuad(namedNode(component.uri), namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), namedNode('http://semcom.digita.ai/voc#component'),),
+      createQuad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#label'), literal(component.label)),
+      createQuad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#description'),  literal(component.description)),
+      createQuad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#author'), literal(component.author)),
+      createQuad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#version'), literal(component.version)),
+      createQuad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#latest'), literal(String(component.latest))),
+      createQuad(namedNode(component.uri), namedNode('http://semcom.digita.ai/voc#tag'), literal(component.tag)),
       ...shapeQuads,
     ];
 
@@ -58,14 +72,70 @@ export class ComponentTransformerService {
   }
 
   /**
-   * Transforms quads to a Component
+   * Retrieves the metadata of a component from an array of quads.
    *
-   * @param quads The quads to transform
+   * @param quads - The quads to transform
+   * @param subject - The URI of the componentMetadata
    */
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  fromQuads(quads: Quad[]): ComponentMetadata {
+  fromQuadsOne(quads: Quad[], uri: string): ComponentMetadata {
 
-    throw new Error('Not implemented');
+    this.logger.log('debug', 'Transforming quads into component');
+    this.logger.log('silly', '', quads.map((quad) => `${quad.subject.value} ${quad.predicate.value} ${quad.object.value}`));
+
+    // maps all predicates for the given URI to its corresponding object
+    const uriTriples = new Map<string, string[]>([ ...requiredPredicates ].map((predicate) => [ predicate, [] ]));
+
+    quads
+      .filter((quad) => quad.subject.value === uri)
+      .forEach((element) => uriTriples.get(element.predicate.value)?.push(element.object.value));
+
+    if ([ ...uriTriples.values() ].some((preds) => preds.length === 0)) {
+
+      throw new Error('Some ComponentMetadata predicates were missing while parsing quads.');
+
+    }
+
+    if ([ ...uriTriples.entries() ].some(([ subject, preds ]) => subject !== `${digitaPrefix}shape` && preds.length > 1)) {
+
+      throw new Error('Too many ComponentMetadata predicates were provided while parsing quads.');
+
+    }
+
+    const latest = uriTriples.get(`${digitaPrefix}latest`)?.[0];
+
+    if (latest !== 'true' && latest !== 'false') {
+
+      throw new Error(`'${digitaPrefix}latest' should be 'true' or 'false'`);
+
+    }
+
+    return {
+      uri,
+      label: uriTriples.get(`${digitaPrefix}label`)?.[0] ?? '',
+      description: uriTriples.get(`${digitaPrefix}description`)?.[0] ?? '',
+      author: uriTriples.get(`${digitaPrefix}author`)?.[0] ?? '',
+      tag: uriTriples.get(`${digitaPrefix}tag`)?.[0] ?? '',
+      version: uriTriples.get(`${digitaPrefix}version`)?.[0] ?? '',
+      latest: uriTriples.get(`${digitaPrefix}latest`)?.[0] === 'true',
+      shapes: uriTriples.get(`${digitaPrefix}shape`) ?? [],
+    };
+
+  }
+
+  /**
+   * Transforms quads to Components
+   *
+   * @param quads - The quads to transform
+   */
+  fromQuads(quads: Quad[]): ComponentMetadata[] {
+
+    const typePredicate = namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type');
+    const componentObject = namedNode('http://semcom.digita.ai/voc#component');
+
+    return quads
+      .filter((quad) => quad.predicate.equals(typePredicate) && quad.object.equals(componentObject))
+      .map((quad) => quad.subject.value)
+      .map((uri) => this.fromQuadsOne(quads, uri));
 
   }
 
---
+++ packages/semcom-node/lib/store/services/pod-sync.service.ts
@@ -0,0 +1,98 @@
+import { Handler, TypedKeyValueStore } from '@digita-ai/handlersjs-core';
+import { Parser } from 'n3';
+import { from, Observable, of } from 'rxjs';
+
+export class PodSyncService<S extends string, M extends { [s in S]: string[] }>
+  extends Handler<void, void> {
+
+  /**
+   *
+   * @param storage - key in which the storage is located
+   * @param store - the given store
+   * @param localPod - the URI of the local pod
+   */
+  constructor(
+    private readonly storage: S,
+    private readonly store: TypedKeyValueStore<M>,
+    private readonly localPod: string,
+  ) { super(); }
+
+  canHandle(): Observable<boolean> {
+
+    return of(true);
+
+  }
+
+  private async componentsInPod(uri: string): Promise<Set<string>> {
+
+    try {
+
+      const response = await fetch(uri, { headers: { 'Accept': 'text/turtle' } });
+      const body = await response.text();
+
+      return new Set(
+        response.status === 200
+          ? new Parser({ format: 'Turtle' }).parse(body)
+            .filter((quad) => quad.object.value === 'http://www.w3.org/ns/ldp#Resource')
+            .filter((quad) => quad.subject.value !== '')
+            .map((quad) => quad.subject.value)
+          : undefined
+      );
+
+    } catch (error) {
+
+      return new Set();
+
+    }
+
+  }
+
+  private async synchronizeComponents(): Promise<void> {
+
+    const existingComponents = await this.componentsInPod(this.localPod);
+
+    const pods: string[] = await this.store.get(this.storage) ?? [];
+
+    // map of components to be fetched, mapped to a node which has the component metadata
+    const componentToFetch: Map<string, string> = new Map();
+
+    await Promise.all(pods.map(async (pod) => {
+
+      [ ...await this.componentsInPod(pod) ]
+        .filter((component) => !existingComponents.has(component))
+        .forEach((component) => componentToFetch.set(component, pod));
+
+    }));
+
+    await Promise.all([ ... componentToFetch.entries() ].map(async ([ component, node ]) => {
+
+      const httpResponse = await fetch(`${node}${component}`, {
+        headers: { 'Accept': 'text/turtle' },
+      });
+
+      if (httpResponse.status === 200) {
+
+        const metadata = await httpResponse.text();
+
+        await fetch(this.localPod, {
+          method: 'post',
+          body: metadata,
+          headers: {
+            'Content-Type': 'text/turtle',
+            'Slug': component, // this is actually an encoded version of the component
+          },
+        });
+
+      }
+
+    }));
+
+  }
+
+  handle(): Observable<void> {
+
+    return from(this.synchronizeComponents());
+
+  }
+
+}
