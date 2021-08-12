b69bd1476b3089135bff7801bdb1ed71b566f160
AbelVandenBriel <abel.van.den.briel@digita.ai>
2021-08-12 17:00:19 +0200

    chore: removed unnecessary files

--- packages/semcom-upload-component/lib/models/holder.model.ts
+++
@@ -1,8 +0,0 @@
-import { LDTriple } from './triple.model';
-
-export interface Holder {
-  uri: string;
-  exchange?: string;
-  triples?: LDTriple[];
-  shape?: string;
-}
--- packages/semcom-upload-component/lib/models/triple.model.ts
+++
@@ -1,7 +0,0 @@
-import { LDNode } from './node.model';
-
-export interface LDTriple {
-  predicate: string;
-  subject: LDNode;
-  object: LDNode;
-}
