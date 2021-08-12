c32fa4340f6e50049da28a395855843a1968c795
AbelVandenBriel <abel.van.den.briel@digita.ai>
2021-08-12 16:59:49 +0200

    chore: moved custom element definitions to components

--- packages/semcom-demo-upload-component/lib/demo.ts
+++ packages/semcom-demo-upload-component/lib/demo.ts
@@ -1,12 +1,3 @@
-import { AuthenticateComponent, ProviderListComponent, LoadingComponent, FeedbackComponent } from '@digita-ai/ui-transfer-components';
-import { FormElementComponent } from '@netwerk-digitaal-erfgoed/solid-crs-components';
-import { SemComRegisterComponent, SemComStoreSelectionComponent, SemComUploadFormComponent } from '@digita-ai/semcom-upload-component';
+import { SemComRegisterComponent } from '@digita-ai/semcom-upload-component';
 
 customElements.define('sem-com-register-component', SemComRegisterComponent);
-customElements.define('auth-flow', AuthenticateComponent);
-customElements.define('provider-list', ProviderListComponent);
-customElements.define('loading-component', LoadingComponent);
-customElements.define('feedback-component', FeedbackComponent);
-customElements.define('sem-com-store-selection', SemComStoreSelectionComponent);
-customElements.define('sem-com-upload-form', SemComUploadFormComponent);
-customElements.define('form-element-component', FormElementComponent);
