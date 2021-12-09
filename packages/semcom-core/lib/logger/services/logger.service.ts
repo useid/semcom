dae5b9fc94e90ebdbcdb0ec8e2e48853d0f3902c
JasperVandenberghen <jasper.vandenberghen@gmail.com>
2021-12-09 12:29:56 +0100

    docs: fixed typo in tsdocs

--- packages/semcom-core/lib/logger/services/logger.service.ts
+++ packages/semcom-core/lib/logger/services/logger.service.ts
@@ -3,11 +3,13 @@
  */
 export abstract class LoggerService {
 
+  /*
    * Logs messages and an optional payload, depending on the severity level.
    *
    * @param { string } level - The log level (severity).
    * @param { string } message - The message to log.
    * @param { any } payload (optional) - The payload to attach to the log.
+   */
   public abstract log(level: string, message: string, payload?: any): void;
 
 }
