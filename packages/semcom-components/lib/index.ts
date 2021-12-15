91563e0f6af460a5d772066cba3c81375dee25a0
Wouter Termont <woutermont@gmail.com>
2021-12-15 12:38:23 +0100

    build: release component package
    Signed-off-by: Wouter Termont <woutermont@gmail.com>

--- packages/semcom-components/lib/index.ts
+++ packages/semcom-components/lib/index.ts
@@ -2,3 +2,4 @@ export * from './components/profile.component';
 export * from './components/payslip.component';
 export * from './components/input.component';
 export * from './components/gender.component';
+export * from './components/base.component';
